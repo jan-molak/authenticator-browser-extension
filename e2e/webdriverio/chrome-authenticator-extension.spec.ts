39e5da17caf2e6e6421f712325513538b930c11d
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2021-08-22 20:56:02 +0200

    ci(github): debugging a failed test

--- e2e/webdriverio/chrome-authenticator-extension.spec.ts
+++ e2e/webdriverio/chrome-authenticator-extension.spec.ts
@@ -4,7 +4,7 @@ import 'mocha';
 import 'webdriverio';
 
 import { Ensure, equals } from '@serenity-js/assertions';
-import { Actor, actorCalled, Cast, engage } from '@serenity-js/core';
+import { Actor, actorCalled, Cast, engage, Log, Question } from '@serenity-js/core';
 import { LocalServer, ManageALocalServer, StartLocalServer } from '@serenity-js/local-server';
 import { BrowseTheWeb, by, Navigate, Target, Text } from '@serenity-js/webdriverio';
 
@@ -32,6 +32,7 @@ describe('Authenticator Browser Extension, when used with WebDriver.io,', functi
     it(`enables a web browser-based test to authenticate with a web app`, () =>
         actorCalled('Dave').attemptsTo(
             Navigate.to(LocalServer.url()),
+            Log.the(BrowserDom()),
             Ensure.that(Text.of(TestPage.Title), equals('Authenticated!')),
         ));
 });
@@ -39,3 +40,8 @@ describe('Authenticator Browser Extension, when used with WebDriver.io,', functi
 const TestPage = {
     Title: Target.the('header').located(by.css('h1')),
 };
+
+const BrowserDom = () =>
+    Question.about('browser DOM', actor => {
+        return BrowseTheWeb.as(actor).browser.$('body').getHTML(true);
+    });
